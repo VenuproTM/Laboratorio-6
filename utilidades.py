0fa6f3ad82a64b8aed743ecbb709ae6da206e2ef
JuanSanchez8678866 <prom22sanchezjuan.sa@gmail.com>
2026-06-19 14:19:12 -0500

    Add files via upload

--- utilidades.py
+++ utilidades.py
@@ -4,12 +4,22 @@ def mostrar_bienvenida():
     """Muestra un mensaje de bienvenida."""
     print("¡Bienvenido al sistema de funciones!")
 
-
 def calcular_promedio(num1, num2):
     """Recibe dos números y devuelve su promedio."""
     return (num1 + num2) / 2
 
-
 def validar_correo(correo):
     patron = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     return bool(re.match(patron, correo))
+
+def calcular_suma(num1, num2):
+    """Recibe dos números y devuelve su suma."""
+    return num1 + num2
+
+def calcular_resta(num1, num2):
+    """Recibe dos números y devuelve la resta (num1 - num2)."""
+    return num1 - num2
+
+def calcular_multiplicacion(num1, num2):
+    """Recibe dos números y devuelve su multiplicación."""
+    return num1 * num2
